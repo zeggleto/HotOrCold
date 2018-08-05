826dfb0fb82a956f88873f9fe446bb086c3c47ed
Zach Eggleton <zeggleto@outlook.com>
2018-08-05 16:26:12 -0400

    Ready for Pi testing

--- Data.py
+++ Data.py
@@ -1,25 +1,76 @@
 import requests
 import json
 
-def get_local_temp(lat, long):
+cities = {}
+temp_is_hot = True
+
+
+def update_weather():
+    cities['local'] = update_local_weather()
+    cities['hightemp'] = update_city('hightemp')
+    cities['lowtemp'] = update_city('lowtemp')
+    cities['highhumid'] = update_city('highhumid')
+    cities['lowhumid'] = update_city('lowhumid')
+    cities['heat'] = update_city('heat')
+    cities['chill'] = update_city('chill')
+
+
+def update_local_weather():
+    local = get_local_city()
+    lat = local['latitude']
+    long = local['longitude']
     url = "http://localhost:5000/localweather"
+    response = requests.post(url, json={'lat': lat, 'long': long})
+    if response.status_code == 200:
+        weather = json.loads(response.content.decode('utf-8'))
+        weather['name'] = local['city']
+        weather['state'] = local['region_code']
+        return weather
+    else:
+        return False
+
+
+def update_city(category):
+    url = 'http://localhost:5000/singleweather/' + category
     headers = {'Content-Type': 'application/json'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        print(json.loads(response.content.decode('utf-8')))
+        city = json.loads(response.content.decode('utf-8'))
+        return city
     else:
-        print("Unable to locate user's geo-coordinates")
+        return False
+
 
 def get_local_city():
     url = 'http://api.ipstack.com/check?access_key=4e664fea892397e28b7c547522ac3f48'
     headers = {'Content-Type': 'application/json'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        print(json.loads(response.content.decode('utf-8')))
+        city = json.loads(response.content.decode('utf-8'))
+        return city
     else:
-        print("Unable to locate user's geo-coordinates")
+        return False
+
+
+def get_city(category):
+    return {
+        'local': cities['local'],
+        'hightemp': cities['hightemp'],
+        'lowtemp': cities['lowtemp'],
+        'highhumid': cities['highhumid'],
+        'lowhumid': cities['lowhumid']
+    }.get(category, {'response': cities['local']})
+
+
+def switch_temp():
+    global temp_is_hot
+    temp_is_hot = not temp_is_hot
+
+    if temp_is_hot:
+        return cities['hightemp']
+    else:
+        return cities['lowtemp']
 
 
 if __name__ == '__main__':
     pass
-    # get_local_city()
--- View.py
+++ View.py
@@ -1,52 +1,220 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.properties import StringProperty
 from kivy.uix.floatlayout import FloatLayout
-from kivy.graphics import Color, Rectangle
-from Data import get_local_temp
+from kivy.graphics import Rectangle
+from kivy.clock import Clock
+import Data
+
+Builder.load_string('''
+<Interface>:
+    AnchorLayout:
+        anchor_x: 'left'
+        anchor_y: 'top'
+        pos: 25, 20
+        Label:
+            canvas.before:
+                Color:
+                    rgba: 0,0,0,0.5
+                Rectangle:
+                    pos: self.pos[0], self.pos[1] - 45
+                    size: self.size
+            size_hint: None, None
+            size: 180, 120
+            text: 'Your Weather'
+        Label:
+            id: local_temp
+            size_hint: None, None
+            size: 180, 210
+            text: root.local
+            font_size: 60
+        Label:
+            id: local_name
+            size: 180, 300
+            size_hint: None, None
+            text: root.city
+            
+    AnchorLayout:
+        anchor_x: 'right'
+        anchor_y: 'top'
+        pos: -25, 20
+        
+        Label:
+            canvas.before:
+                Color:
+                    rgba: 0,0,0,0.5
+                Rectangle:
+                    pos: self.pos[0], self.pos[1] - 45
+                    size: self.size
+            size_hint: None, None
+            size: 180, 120
+            text: 'Still better than'
+        Label:
+            id: compare_temp
+            size_hint: None, None
+            size: 180, 210
+            text: root.local
+            font_size: 60
+        Label:
+            id: compare_name
+            size: 180, 300
+            size_hint: None, None
+            text: root.city
+            
+    AnchorLayout:
+        anchor_x: 'center'
+        anchor_y: 'top'
+        pos: 0, -20
+        Label: 
+            id: updating
+            size: 0, -100
+            size_hint: None, None
+            text: 'Updating...'
+            
+    AnchorLayout:
+        anchor_x: 'left'
+        anchor_y: 'center'
+        pos: 50, 100
+        
+        Label:
+            canvas.before:
+                Color:
+                    rgba: 0,0,0,0.5
+                Rectangle:
+                    pos: self.pos[0], self.pos[1] - 45
+                    size: self.size[0] + 50, self.size[1]
+            size: 80, 120
+            size_hint: None, None
+            text: 'Humidity'
+        Label:
+            id: local_humid
+            size_hint: None, None
+            size: 225, 120
+            text: root.local
+            
+    AnchorLayout:
+        anchor_x: 'left'
+        anchor_y: 'center'
+        pos: 50, 80
+        Label:
+            size: 52, 120
+            size_hint: None, None
+            text: 'Heat'
+        Label:
+            id: local_heat
+            size_hint: None, None
+            size: 225, 120
+            text: root.local
+            
+    AnchorLayout:
+        anchor_x: 'right'
+        anchor_y: 'center'
+        pos: -100, 100
+        Label:
+            canvas.before:
+                Color:
+                    rgba: 0,0,0,0.5
+                Rectangle:
+                    pos: self.pos[0], self.pos[1] - 45
+                    size: self.size[0] + 50, self.size[1]
+            size_hint: None, None
+            size: 80, 120
+            text: 'Humidity:'
+        Label:
+            id: compare_humid
+            size_hint: None, None
+            size: -65, 120
+            text: root.city
+            
+    AnchorLayout:
+        anchor_x: 'right'
+        anchor_y: 'center'
+        pos: -50, 80
+        Label:
+            size: 212, 120
+            size_hint: None, None
+            text: 'Heat'
+        Label:
+            id: compare_heat
+            size_hint: None, None
+            size: 36, 120
+            text: root.local
+            
+    AnchorLayout:
+        anchor_x: 'left'
+        anchor_y: 'bottom'
+        
+        Button: 
+            id: hot_cold
+            text: 'Hot'
+            on_press: root.switch_temp()
+            size: 100, 100
+            size_hint: None, None
+''')
 
 
 class Interface(FloatLayout):
+    local = city = StringProperty()
+
     def __init__(self, **kwargs):
         super(Interface, self).__init__(**kwargs)
-        self.your_temp = get_local_temp()
-        self.your_location = 'Plainfield, IN'
-        self.add_widget(Label(
-            text='Your Weather',
-            size_hint=(.2,1.75),
-            text_size=self.size,
-            halign='left',
-            valign='top'
-        ))
-        self.add_widget(Label(
-            text=str(self.your_temp),
-            size_hint=(.2, 1.70),
-            text_size=self.size,
-            font_size=60,
-            halign='left',
-            valign='top'
-        ))
-        self.add_widget(Label(
-            text=self.your_location,
-            size_hint=(.2, 1.5),
-            text_size=self.size,
-            halign='left',
-            valign='top'
-        ))
+        self.local = "--"
+        self.city = "--"
+        self.hot = True
+
+    def update_interface(self):
+        self.ids.updating.size = (0, 0)
+        Data.update_weather()
+        self.get_local()
+        self.get_temp(self.hot)
+        self.ids.updating.size = (0, -100)
+
+    def get_local(self):
+        local = Data.get_city('local')
+        self.ids.local_temp.text = str(int(local['main']['temp']))
+        self.ids.local_name.text = local['name'] + ", " + local['state']
+        self.ids.local_humid.text = str(local['main']['humidity'])
+        self.ids.local_heat.text = str(int(local['main']['heat']))
+
+    def get_temp(self, hot):
+        if hot is True:
+            compare = Data.get_city('hightemp')
+            self.ids.compare_temp.text = str(int(compare['main']['temp']))
+            self.ids.compare_name.text = compare['name'] + ", " + compare['state']
+            self.ids.compare_humid.text = str(compare['main']['humidity'])
+            self.ids.compare_heat.text = str(int(compare['main']['heat']))
+        else:
+            compare = Data.get_city('lowtemp')
+            self.ids.compare_temp.text = str(int(compare['main']['temp']))
+            self.ids.compare_name.text = compare['name'] + ", " + compare['state']
+            self.ids.compare_humid.text = str(compare['main']['humidity'])
+            self.ids.compare_heat.text = str(int(compare['main']['heat']))
+
+    def switch_temp(self):
+        self.get_temp(not self.hot)
+        if self.hot is True:
+            self.ids.hot_cold.text = "Cold"
+        else:
+            self.ids.hot_cold.text = "Hot"
+        self.hot = not self.hot
 
 
 class WeatherCompared(App):
     def build(self):
         self.root = root = Interface()
-        root.bind(size=self._update_rect, pos=self._update_rect)
+        root.bind(size=self._update_rect)
 
         with root.canvas.before:
-            Color(0.2, 0.2, 1)
-            self.rect = Rectangle(size=root.size, pos=root.pos)
+            self.rect = Rectangle(size=root.size)
         return root
 
+    def on_start(self, **kwargs):
+        self.root.update_interface()
+        Clock.schedule_interval(lambda dt: self.root.update_interface(), 660)
+
     def _update_rect(self, instance, value):
-        self.rect.pos = instance.pos
         self.rect.size = instance.size
+        self.rect.source = 'Images/Emerald Lake.jpg'
 
 
 if __name__ == '__main__':
